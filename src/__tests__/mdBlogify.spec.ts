50732ee2ba093553c5e14735030f06b8c4d85d5d
ryou <visualjupiter@gmail.com>
2019-08-24 10:51:46 +0900

    fix: previewの算出が正常にできていなかった不具合の修正

--- src/__tests__/mdBlogify.spec.ts
+++ src/__tests__/mdBlogify.spec.ts
@@ -9,14 +9,14 @@ import { makeArticleInfo } from '../mdBlogify'
 describe('makeArticleInfo', () => {
     test('一般的なパターン', () => {
         const result = makeArticleInfo(
-            '---\ncreated_at: 2019-08-15\n---\n\n# タイトル\n## 最初の見出し\nサンプルテキスト\n',
+            '---\ncreated_at: 2019-08-15\n---\n\n# タイトル\n## 最初の見出し\n### サンプルテキスト\n',
             10,
             'sample-article'
         )
         expect(result).toEqual({
             articleData: {
                 title: 'タイトル',
-                bodyHtml: '<h2>最初の見出し</h2>\n<p>サンプルテキスト</p>\n',
+                bodyHtml: '<h2>最初の見出し</h2>\n<h3>サンプルテキスト</h3>\n',
                 frontmatters: {
                     created_at: '2019-08-15',
                 },
